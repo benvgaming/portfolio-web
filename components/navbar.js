38e809233cdf3092889a727605703c53b7247122
unknown <benvgaming@pm.me>
2023-04-26 18:54:42 +0700

    update navbar but its broken, need to fix later

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,6 @@
 import Logo from './logo'
 import NextLink from 'next/link'
+import {forwardRef} from 'react'
 import {
     Container,
     Box,
@@ -17,37 +18,39 @@ import {
 
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
-//import { IoLogoGithub } from 'react-icons/io5'
+import { IoLogoGithub } from 'react-icons/io5'
 
 
-const LinkItem = ({href,path,children}) => {
+const LinkItem = ({ href, path, children }) => {
     const active = path === href;
     const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900');
     return (
         <NextLink href={(href)}>
-            <Link 
+            <Link
                 p={2}
                 bg={active ? 'glassTeal' : undefined}
-                color={active ? '$202023' : inactiveColor }
+                color={active ? '$202023' : inactiveColor}
             >
-             {children}   
+                {children}
             </Link>
         </NextLink>
     )
 }
-
+const MenuLink = forwardRef((props, ref) => (
+    <Link ref={ref} as={NextLink} {...props} />
+))
 const Navbar = props => {
-    const {path} = props;
-     
+    const { path } = props;
+
     return (
         <Box
-        position="fixed"
-        as="nav"
-        w="100%"
-        bg={useColorModeValue('#ffffff40', '#20202380')}
-        css={{ backdropFilter: 'blur(10px)' }}
-        zIndex={2}
-        {...props}
+            position="fixed"
+            as="nav"
+            w="100%"
+            bg={useColorModeValue('#ffffff40', '#20202380')}
+            css={{ backdropFilter: 'blur(10px)' }}
+            zIndex={2}
+            {...props}
         >
             <Container
                 display="flex"
@@ -59,16 +62,16 @@ const Navbar = props => {
             >
                 <Flex align="center" mr={5}>
                     <Heading as="h1" size="lg" letterSpacing={'tighter'}>
-                        <Logo/>
+                        <Logo />
                     </Heading>
                 </Flex>
                 <Stack
-                    direction={{base: 'column', md: 'row'}}
-                    display={{base:'none', md:'flex'}}
-                    width={{ base:'full', md: 'auto'}}
+                    direction={{ base: 'column', md: 'row' }}
+                    display={{ base: 'none', md: 'flex' }}
+                    width={{ base: 'full', md: 'auto' }}
                     alignItems="center"
                     flexGrow={1}
-                    mt={{base:4, nmd:0}}
+                    mt={{ base: 4, nmd: 0 }}
                 >
                     <LinkItem href="/works" path={path}>
                         Works
@@ -76,28 +79,40 @@ const Navbar = props => {
                     <LinkItem href="/posts" path={path}>
                         Posts
                     </LinkItem>
+                    <LinkItem
+                        target="_blank"
+                        href="https://github.com/benvgaming/portfolio-web"
+                        path={path}
+                        display="inline-flex"
+                        alignItems="center"
+                        style={{ gap: 4 }}
+                        pl={2}
+                    >
+                        <IoLogoGithub />
+                        Source
+                    </LinkItem>
                 </Stack>
 
                 <Box flex={1} align="right">
-                    <ThemeToggleButton/>
-                    <Box ml={2} display={{base:'inline-block', md:'none'}}>
-                        <Menu>
-                            <MenuButton as={IconButton} 
-                            icon={<HamburgerIcon/>} 
-                            variant="outline" 
-                            aria-label="Options"
+                    <ThemeToggleButton />
+                    <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+                        <Menu isLazy id="navbar-menu">
+                            <MenuButton as={IconButton}
+                                icon={<HamburgerIcon />}
+                                variant="outline"
+                                aria-label="Options"
                             />
                             <MenuList>
                                 <NextLink href="/" passHref>
-                                    <MenuItem as={Link}>About</MenuItem>
+                                    <MenuItem as={MenuLink}>About</MenuItem>
                                 </NextLink>
                                 <NextLink href="/posts" passHref>
-                                    <MenuItem as={Link}>Posts</MenuItem>
+                                    <MenuItem as={MenuLink}>Posts</MenuItem>
                                 </NextLink>
                                 <NextLink href="/works" passHref>
-                                    <MenuItem as={Link}>Works</MenuItem>
+                                    <MenuItem as={MenuLink}>Works</MenuItem>
                                 </NextLink>
-                                <MenuItem as={Link} href="https://www.cratz.dog/">View Source</MenuItem>
+                                <MenuItem as={MenuLink} href="https://github.com/benvgaming/portfolio-web">View Source</MenuItem>
                             </MenuList>
                         </Menu>
                     </Box>
